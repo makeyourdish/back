4c0323d8a8f9c09f00e8ea36f2c3f391e781f498
Romain <romainbidault@RomainM2.lan>
2022-11-27 16:56:38 +0100

    Adding Sign up/in route and auth middleware

--- src/session.js
+++ src/session.js
@@ -1,10 +1,22 @@
+import auth from "./middleware/auth.js";
 import { ExempleGetIngredients } from "./route/exemple.js";
+import { userSession, userSignIn, userSignUp } from "./route/Users.js";
 
 
 const sessionRoutes = ({ app }) => {
 //ALL definition route are here
-app.get("/ingredients", async (req, res) => {
+app.get("/ingredients", auth, async (req, res) => {
     await ExempleGetIngredients(req, res);
+  });
+
+  app.post("/sign-in", async (req, res) => {
+    await userSignIn(req, res);
+  });
+  app.post("/sign-up", async (req, res) => {
+    await userSignUp(req, res);
+  });
+  app.get("/session", async (req, res) => {
+    await userSession(req, res);
   });
 };
 
