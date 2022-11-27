b405fc9fd4e3730db9a535483b2bf09adc1a75e7
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-11-28 00:38:21 +0100

    add eslint & prettier

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-import express from "express";
-import sessionRoutes from "./src/session.js";
-import cors from "cors";
-import "dotenv/config";
-import morgan from "morgan";
-import pino from "pino";
+import express from "express"
+import sessionRoutes from "./src/session.js"
+import cors from "cors"
+import "dotenv/config"
+import morgan from "morgan"
+import pino from "pino"
 
 const logger = pino()
 
-const app = express();
-const port = process.env.SERVER_PORT;
+const app = express()
+const port = process.env.SERVER_PORT
 
 app.options(
   "*",
-  cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 })
-);
+  cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }) // todo: change with .env variables (https://makeyourdish.vercel.app)
+)
 
-app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 }));
-app.use(morgan("dev"));
-app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", optionsSuccessStatus: 200 })) // todo: change with .env variables (https://makeyourdish.vercel.app)
+app.use(morgan("dev"))
+app.use(express.json())
 
-sessionRoutes({ app });
+sessionRoutes({ app })
 
-app.listen(port, () => logger.info(`🍹 Listening on :${port}`));
+app.listen(port, () => logger.info(`🍹 Listening on :${port}`))
