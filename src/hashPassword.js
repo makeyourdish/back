6344ea1b39eb0ca8eec5f492c52941d5432f5c2e
DylanGil <dylan.flame.libra@gmail.com>
2022-11-30 22:25:47 +0100

    signin signup working

--- src/hashPassword.js
+++ src/hashPassword.js
@@ -1,8 +1,9 @@
-import { pbkdf2Sync, randomBytes } from "crypto";
+import { pbkdf2Sync, randomBytes } from "crypto"
 
 const hashPassword = (password, salt = randomBytes(1000).toString("hex")) => {
-  const passwordHash = pbkdf2Sync(password, salt, 100000, 16, "sha512");
-  return [passwordHash.toString("hex"), salt];
-};
+  const passwordHash = pbkdf2Sync(password, salt, 100000, 16, "sha512")
 
-export default hashPassword;
+  return [passwordHash.toString("hex"), salt]
+}
+
+export default hashPassword
