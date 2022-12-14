73d4bcd15abdb671909d5786ec64ab96e1098989
Romain <romainbidault@RomainM2.local>
2022-12-14 08:41:55 +0100

    Add middleware to private route for admin user

--- src/session.js
+++ src/session.js
@@ -1,4 +1,5 @@
 import auth from "./middleware/auth.js";
+import authMaster from "./middleware/authMaster.js";
 import { CreateCategoryIngredient, DeleteCategoryIngredient, GetCategoryIngredients, UpdateCategoryIngredient } from "./route/CategoryIngredient.js";
 import { ExempleGetIngredients } from "./route/exemple.js";
 import { CreateIngredients, DeleteIngredient, GetIngredient, GetIngredients, UpdateIngredient } from "./route/Ingredients.js";
@@ -24,61 +25,61 @@ app.get("/exemple/ingredients", async (req, res) => {
     await userSession(req, res);
   });
   //Recipe
-  app.get('/allRecipes', async (req,res) => {
+  app.get('/allRecipes', auth, async (req,res) => {
     await GetRecipes(req,res);
   })
-  app.get('/recipe/:idRecipe', async (req,res) => {
+  app.get('/recipe/:idRecipe', auth, async (req,res) => {
     await GetRecipe(req,res);
   })
-  app.post('/recipe', async (req,res) => {
+  app.post('/recipe', authMaster,async (req,res) => {
     await CreateRecipes(req,res);
   })
-  app.put('/recipe', async (req,res) => {
+  app.put('/recipe', authMaster, async (req,res) => {
     await UpdateRecipes(req,res);
   })
-  app.delete('/recipe', async (req,res) => {
+  app.delete('/recipe', authMaster, async (req,res) => {
     await DeleteRecipe(req,res);
   })
   //ingredient
-  app.get('/allIngredient', async (req,res) => {
+  app.get('/allIngredient', auth, async (req,res) => {
     await GetIngredients(req,res);
   })
-  app.get('/ingredient/:idIngredient', async (req,res) => {
+  app.get('/ingredient/:idIngredient', auth, async (req,res) => {
     await GetIngredient(req,res);
   })
-  app.post('/ingredient', async (req,res) => {
+  app.post('/ingredient', authMaster, async (req,res) => {
     await CreateIngredients(req,res);
   })
-  app.put('/ingredient', async (req,res) => {
+  app.put('/ingredient', authMaster, async (req,res) => {
     await UpdateIngredient(req,res);
   })
-  app.delete('/ingredient', async (req,res) => {
+  app.delete('/ingredient', authMaster, async (req,res) => {
     await DeleteIngredient(req,res);
   })
   //CategoryIngredient
-  app.get('/allCategoryIngredient', async (req,res) => {
+  app.get('/allCategoryIngredient', auth, async (req,res) => {
     await GetCategoryIngredients(req,res);
   })
-  app.post('/categoryIngredient', async (req,res) => {
+  app.post('/categoryIngredient', authMaster, async (req,res) => {
     await CreateCategoryIngredient(req,res);
   })
-  app.put('/categoryIngredient', async (req,res) => {
+  app.put('/categoryIngredient', authMaster, async (req,res) => {
     await UpdateCategoryIngredient(req,res);
   })
-  app.delete('/categoryIngredient', async (req,res) => {
+  app.delete('/categoryIngredient', authMaster, async (req,res) => {
     await DeleteCategoryIngredient(req,res);
   })
   //RecipeType
-  app.get('/allRecipeType', async (req,res) => {
+  app.get('/allRecipeType', auth, async (req,res) => {
     await GetRecipeType(req,res);
   })
-  app.post('/recipeType', async (req,res) => {
+  app.post('/recipeType', authMaster, async (req,res) => {
     await CreateRecipeType(req,res);
   })
-  app.put('/recipeType', async (req,res) => {
+  app.put('/recipeType', authMaster, async (req,res) => {
     await UpdateRecipeType(req,res);
   })
-  app.delete('/recipeType', async (req,res) => {
+  app.delete('/recipeType', authMaster, async (req,res) => {
     await DeleteRecipeType(req,res);
   })
 };
